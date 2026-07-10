b021bae16b2849868b5469c47ac3948ec0b3e92f
vsuchkov <vsuchkov@axbit.ru>
2026-07-10 15:59:26 +0300

    Fix packaging: install top-level modules so 'pyvpn' console script works

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import find_packages, setup
 
+TOP_LEVEL_MODULES = [
+    "app",
+    "cli",
+    "config",
+    "server",
+    "client",
+    "routing",
+    "tun",
+    "status",
+    "privileges",
+]
+
 setup(
     name="pyvpn",
     version="0.1.0",
     packages=find_packages(where="src"),
+    py_modules=TOP_LEVEL_MODULES,
     package_dir={"": "src"},
     python_requires=">=3.10",
     install_requires=[
